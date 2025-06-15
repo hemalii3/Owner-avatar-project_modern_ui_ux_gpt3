86c6936fd835471f1e439c46b77bc392fd2cc5e0
hemali S. <hemalisuthar.work@gmail.com>
2025-06-15 18:59:45 +0200

    Update imports.js

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -4,6 +4,7 @@ import atlassian from '../../assets/atlassian.png';
 import dropbox from '../../assets/dropbox.png';
 import shopify from '../../assets/shopify.png';
 
+
 export {
   google,
   slack,
